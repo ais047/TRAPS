4d94fa397cdc298cbb48caa3d4a81d790725d3f1
ais047 <hursungwoo@yahoo.com>
2018-07-26 15:43:50 -0700

    dad

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <MyMaps :markers='[
+        {"lat":35.6432027,"lng":139.6729435},
+        {"lat":35.5279833,"lng":139.6989209},
+        {"lat":35.6563623,"lng":139.7215211},
+        {"lat":35.6167531,"lng":139.5469376},
+        {"lat":35.6950961,"lng":139.5037899}
+      ]'></MyMaps>
     <router-view/>
   </div>
 </template>
 
 <script>
+import MyMaps from './components/MyMap'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MyMaps
+  }
 }
 </script>
 
---
+++ src/components/MapLoader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div id="map"></div>
+    <template v-if="!!this.google && !!this.map">
+      <slot
+        :google="google"
+        :map="map"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+import GoogleMapsApiLoader from 'google-maps-api-loader'
+import MapProvider from './MapProvider'
+
+export default {
+  props: {
+    mapConfig: Object,
+    apiKey: String
+  },
+  components: {
+    MapProvider
+  },
+  data () {
+    return {
+      google: null,
+      map: null
+    }
+  },
+  mounted () {
+    GoogleMapsApiLoader({
+      apiKey: this.apiKey
+    }).then((google) => {
+      this.google = google
+      this.initializeMap()
+    })
+  },
+  methods: {
+    initializeMap () {
+      const mapContainer = this.$el.querySelector('#map') // point 1
+      const { Map } = this.google.maps
+      this.map = new Map(mapContainer, this.mapConfig)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#map {
+  height: 100vh;
+  width: 100%;
+}
+</style>
